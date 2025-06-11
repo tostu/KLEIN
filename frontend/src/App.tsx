3c6918e55445e28c0656065ab32dfa380b767ba2
torge <torge.stubbe@fricke.de>
2025-06-11 15:34:43 +0200

    fallback base url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -66,7 +66,7 @@ function App() {
   );
 
   // Configure your Hono backend URL
-  const HONO_BASE_URL = import.meta.env.VITE_HONO_BASE_URL as string;
+  const HONO_BASE_URL = import.meta.env.VITE_HONO_BASE_URL || "/";
 
   const supportedFormats: SupportedFormat[] = [
     { extension: "jpeg", mimeType: "image/jpeg", quality: 0.9 },
