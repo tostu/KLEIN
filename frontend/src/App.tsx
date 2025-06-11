01779b73c28aee8e2469c2a48f3a6dd93814e792
tostu <torge.stubbe@googlemail.com>
2025-06-11 23:46:47 +0200

    remove base again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -66,10 +66,10 @@ function App() {
   );
 
   // Configure your Hono backend URL
-  const HONO_BASE_URL =
-    import.meta.env.MODE === "development"
-      ? import.meta.env.VITE_HONO_BASE_URL
-      : null;
+  // const HONO_BASE_URL =
+  //   import.meta.env.MODE === "development"
+  //     ? import.meta.env.VITE_HONO_BASE_URL
+  //     : null;
 
   const supportedFormats: SupportedFormat[] = [
     { extension: "jpeg", mimeType: "image/jpeg", quality: 0.9 },
@@ -166,7 +166,7 @@ function App() {
     formData.append("files[]", blob, filename);
 
     try {
-      const response = await fetch(`${HONO_BASE_URL}/upload`, {
+      const response = await fetch(`/upload`, {
         method: "POST",
         mode: "cors",
         body: formData,
